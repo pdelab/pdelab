881a12baabe593dff93b55b11a8061723a91f7c5
Maximilian Metti <maximilianmetti@gmail.com>
2021-05-04 00:33:33 -0400

    minimal example with c call to dolfin

---
+++ assets/test/README.md
@@ -0,0 +1,6 @@
+ # Example for SWIG
+
+ Call:
+```
+  swig -python -c++ example.i && python3 setup.py build_ext
+```
---
+++ assets/test/example.h
@@ -0,0 +1,8 @@
+#ifndef __EXAMPLE_H
+#define __EXAMPLE_H
+
+int fact(int n);
+
+int create_mesh(int grid_size);
+
+#endif
